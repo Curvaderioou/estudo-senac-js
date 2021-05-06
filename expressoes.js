536cff337166810f9ff3b266f9b9f6e730ee3a1e
Curvaderioou <felipedegenerportella@yahoo.com>
2021-05-06 19:29:52 -0300

    adicionado exercicios de expressoes e matriz

---
+++ expressoes.js
@@ -0,0 +1,14 @@
+var num1 = parseInt(prompt("Digite o primeiro número"));
+var num2 = parseInt(prompt("Digite o segundo número"));
+
+var soma = num1 + num2;
+var divisao = num1 / num2;
+var multiplicar = num1 * num2;
+var multiDouble = (2*(num1+num2))
+
+console.log(num1);
+console.log(num2);
+console.log(soma);
+console.log(divisao);
+console.log(multiplicar);
+console.log(multiDouble);
---
+++ matriz.js
@@ -0,0 +1,36 @@
+var media = [[], [], [], [], []]
+var situacao = [[], [], [], [], []]
+
+media [0][0] = "Média";
+media [1][0] = "Português";
+media [2][0] = "Matemática";
+media [3][0] = "Física";
+media [4][0] = "Biologia";
+
+situacao [0][0] = "Média";
+situacao [1][0] = "Português";
+situacao [2][0] = "Matemática";
+situacao [3][0] = "Física";
+situacao [4][0] = "Biologia";
+
+ for (var linha=0 ; linha < media.length  ; linha++ ) {
+  for (var coluna=1 ; coluna < 11 ; coluna++){
+   if (linha==0){
+     media [linha][coluna] = prompt("Insira seu nome: ");
+     situacao [linha][coluna] = media [linha][coluna];
+   } else {
+     media [linha][coluna] = prompt (`Insira a média de  ${media[linha][0]} +  do aluno  ${ media [0] [coluna]}`)};
+    if (media [linha][coluna] < 6) {
+      situacao [linha][coluna] = "Reprovado"
+    } else {
+      situacao [linha] [coluna] = "Aprovado"
+    }
+   }
+  }
+ 
+for (linha = 0 ; linha < situacao.length; linha ++) { 
+  for  (coluna = 0 ; coluna < 11 ; coluna ++){
+    alert (situacao [linha] [coluna]);}  {alert ( "\n" )}; 
+  }
+
+
